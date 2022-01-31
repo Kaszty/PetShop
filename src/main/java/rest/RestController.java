497727c5bd30942deb03b2eaf7d09a6cd7a52e1d
Kaszty <kaszty85@gmail.com>
2022-01-31 21:51:06 +0200

    First add

--- src/main/java/rest/RestController.java
+++ src/main/java/rest/RestController.java
@@ -1,2 +1,12 @@
-package rest;public class RestController {
+package rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+
+    @GetMapping("/get-message")
+    public String getMessage(){
+        return "Wellcome to Spring Rest";
+    }
 }
